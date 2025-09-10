140229511e7bc22ad4bbb9fe00f1fc84f2283a2a
Sébastien Chopin <atinux@gmail.com>
2025-09-11 00:51:07 +0200

    chore: enable analytics

---
+++ app/plugins/analytics.client.ts
@@ -0,0 +1,20 @@
+import { inject, pageview } from '@vercel/analytics'
+
+export default defineNuxtPlugin(() => {
+  const nuxtApp = useNuxtApp()
+  const route = useRoute()
+
+  onNuxtReady(() => {
+    inject({
+      disableAutoTrack: true,
+      framework: 'nuxt',
+    })
+  })
+  // On navigation to a new page
+  nuxtApp.hooks.hook('page:finish', () => {
+    pageview({
+      route: route.matched[0]?.path || route.path,
+      path: route.path,
+    })
+  })
+})
